21857c954a4083bb9b88e72eb4069687aa5c4a5d
LeshaCreator65646 <kalinin091211@icloud.com>
2025-10-31 16:27:07 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
 let speech = new SpeechSynthesisUtterance();
+const downloadButton = document.getElementById('downloadButton');
+
+document.querySelector("#rate").addEventListener("input", () => {
+  const rate = document.querySelector("#rate").value;
+
+  speech.rate = rate;
+
+  document.querySelector("#rate-label").value = rate;
+});
+
+document.querySelector("#pitch").addEventListener("input", () => {
+  const pitch = document.querySelector("#pitch").value;
+ 
+  speech.pitch = pitch;
+
+  document.querySelector("#pitch-label").value = pitch;
+});
+
+document.querySelector("#volume").addEventListener("input", () => {
+  const volume = document.querySelector("#volume").value;
+ 
+  speech.volume = volume;
+
+  document.querySelector("#volume-label").value = volume;
+});
 
 let voices = [];
 
@@ -21,3 +46,20 @@ document.querySelector("button").addEventListener("click", () =>{
     speech.text = document.querySelector("textarea").value;
     window.speechSynthesis.speak(speech);
 });
+
+function download() {
+    if (textInput.value !== '') {
+        const text = encodeURIComponent(textInput.value);
+        const lang = voiceSelect.value;
+        const audioUrl = `https://api.voicerss.org/?key=f42e287e60e148cb87f686f31aa4f1ad&src=${text}&hl=${lang}&r=-3&c=wav`;
+
+        const a = document.createElement('a');
+        a.href = audioUrl;
+        a.download = 'speech.mp3';
+        document.body.appendChild(a);
+        a.click();
+        document.body.removeChild(a);
+    }
+}
+
+downloadButton.addEventListener('click', download);
